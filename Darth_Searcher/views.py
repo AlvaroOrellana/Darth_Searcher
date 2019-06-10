26d7e30c62c3c1eec98709f6a980afd03ae4a7b2
AlvaroOrellana <alvaroorellana9@hotmail.com>
2019-06-10 23:22:24 +0200

    Actualización de Carrusel, icono y ajustes en general.
    Se configura para obtener información de todos los personajes y mostrarla mediante el carrusel.
    Así mismo, se añade una imagen temporal en función del género del personaje.
    Se añade icono.

--- Darth_Searcher/views.py
+++ Darth_Searcher/views.py
@@ -2,6 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import swapi
 
 def index(request):
-		return render(request, "index.html")
+	pjs = swapi.get_all("people")
+	return render(request, "index.html", {'pjs': pjs.items})
+	#pjss = swapi.get_person(5)
+	#pjs = [pjss]
+	#return render(request, "index.html", {'pjs': pjs})
