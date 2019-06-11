874a82e63a510eedb06bcc438934b5a990c22dc3
AlvaroOrellana <alvaroorellana9@hotmail.com>
2019-06-11 11:56:45 +0200

    Cambios estéticos
    Se mejora la interfaz y se retira foto temporal. Así mismo, se añade estilo para diversos componentes y se retira código comentado.

--- Darth_Searcher/views.py
+++ Darth_Searcher/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render
 import swapi
 
 def index(request):
-	pjs = swapi.get_all("people")
-	return render(request, "index.html", {'pjs': pjs.items})
-	#pjss = swapi.get_person(5)
-	#pjs = [pjss]
-	#return render(request, "index.html", {'pjs': pjs})
+	#pjs = swapi.get_all("people")
+	#return render(request, "index.html", {'pjs': pjs.items})
+	pjss = swapi.get_person(5)
+	pjs = [pjss]
+	return render(request, "index.html", {'pjs': pjs})
